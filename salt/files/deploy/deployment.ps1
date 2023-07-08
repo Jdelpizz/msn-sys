4879c90f6db883910243a7c399a0f35e8f842b70
Jdelpizz <user@email.invalid>
2023-07-08 18:24:14 +0200

    Created files for deployment of scheduled tasks Via Salt

---
+++ salt/files/deploy/deployment.ps1
@@ -0,0 +1,9 @@
+# Deploy msn-sys
+# Depending on grain deploy different msn-system
+# Depending on grain deploy different XML
+
+# Fix Scheduled Tasks
+# Needs xml for more granular options
+# Needs /ru so it runs whether user is logged in or not
+# Does not need authors or principles section of XML
+schtasks.exe /Create /TN "\Mission System\server" /XML msn-sys.xml /ru administrator /rp ***
