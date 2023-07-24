05e95ac15940ad0ccce49147d50bceabb2a6767e
John Delpizzo <john.r.delpizzo@gmail.com>
2023-07-24 08:36:41 +0200

    Removed '/' from env

--- dashboard/run_dev_server.ps1
+++ dashboard/run_dev_server.ps1
@@ -13,17 +13,17 @@ echo "Setting Variables"
 $Env:VITE_TAILNUMBERS='tv-01 tv-02 tv-05 tv-06' # Space delimited
 $Env:VITE_WS_MX_URI='10.101.160.60'
 $Env:VITE_WS_MX_PORT='8888'
-$Env:VITE_WS_MX_PATH='/mx-dashboard'
-$Env:VITE_WS_MX_CONTROL_PATH='/mx-controller'
-$Env:VITE_WS_MX_SORTIE_PATH='/mx-sortie-state'
+$Env:VITE_WS_MX_PATH='mx-dashboard'
+$Env:VITE_WS_MX_CONTROL_PATH='mx-controller'
+$Env:VITE_WS_MX_SORTIE_PATH='mx-sortie-state'
 
 # MSN Server
 #$Env:VITE_SHOPS='bcd gccs iamd intel freq wx' # Space delimited
 $Env:VITE_SHOPS='BCD GCCS IAMD INTEL FREQ WX' # Space delimited, KEYS UPPER CASE
 $Env:VITE_WS_MSN_URI='10.101.170.70'
 $Env:VITE_WS_MSN_PORT='8889'
-$Env:VITE_WS_MSN_PATH='/msn-dashboard'
-$Env:VITE_WS_MSN_CONTROL_PATH='/msn-controller'
+$Env:VITE_WS_MSN_PATH='msn-dashboard'
+$Env:VITE_WS_MSN_CONTROL_PATH='msn-controller'
 
 
 echo "Starting Server"
